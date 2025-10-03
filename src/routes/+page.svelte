8417d2bfed44b4f2ebe29cc097ead194b7791a35
Malb7mm <ykmeak9g@gmail.com>
2025-10-03 09:33:00 +0900

    performance.now() test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,31 +6,38 @@
   }[] = $state([]);
 
   let prev = 0;
-  const LOG_MAX = 12;
+  let prev_pf = 0;
+  const LOG_MAX = 16;
 
   onMount(() => {
     document.addEventListener("keydown", (e) => {
-      const now = Math.floor(e.timeStamp);
+      const now = e.timeStamp;
+      const now_pf = performance.now();
       const diff = now - prev;
+      const diff_pf = now_pf - prev_pf;
       logs.push({
-        text: `${now} (+${diff})`
+        text: `${now.toFixed(2)} (+${diff.toFixed(2)}) | ${now_pf.toFixed(2)} (+${diff_pf.toFixed(2)})`
       });
       while (logs.length > LOG_MAX) {
         logs.shift();
       }
       prev = now;
+      prev_pf = now_pf;
     });
     document.addEventListener("pointerdown", (e) => {
       e.preventDefault();
-      const now = Math.floor(e.timeStamp);
+      const now = e.timeStamp;
+      const now_pf = performance.now();
       const diff = now - prev;
+      const diff_pf = now_pf - prev_pf;
       logs.push({
-        text: `${now} (+${diff})`
+        text: `${now.toFixed(2)} (+${diff.toFixed(2)}) | ${now_pf.toFixed(2)} (+${diff_pf.toFixed(2)})`
       });
       while (logs.length > LOG_MAX) {
         logs.shift();
       }
       prev = now;
+      prev_pf = now_pf;
     });
     document.addEventListener("pointermove", (e) => {
       e.preventDefault();
@@ -42,7 +49,7 @@
     入力イベントの分解能確認デモ
   </h1>
   <p>
-    Event.timeStampを取得して表示しています
+    Event.timeStamp (+diff) | Performance.now (+diff)
   </p>
   <p>
     キーボード入力/マウス/タッチ どれでも
